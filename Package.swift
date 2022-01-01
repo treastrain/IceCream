26c99b3adba58b0d68d0fbee7186bafa2c0ae857
treastrain / Tanaka Ryoga <tr@tret.jp>
2022-01-02 00:18:27 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "IceCream",
     platforms: [
-        .macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)
+        .macOS(.v10_12), .iOS(.v11), .tvOS(.v10), .watchOS(.v3)
     ],
     products: [
         .library(
@@ -13,8 +13,8 @@ let package = Package(
     ],
     dependencies: [
         .package(
-            url: "https://github.com/realm/realm-cocoa", 
-            from: "4.1.1"
+            url: "https://github.com/realm/realm-swift",
+            .upToNextMinor(from: "10.7.7")
         )
     ],
     targets: [
